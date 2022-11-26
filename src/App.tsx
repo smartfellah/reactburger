14257c75d1a92ffc4ef14bad4af70ab6ac5e1fcd
smartfellah <smartfellah@outlook.com>
2022-11-26 15:57:33 +0300

    Add IngredientsItem component
    *import BurgerIngredients in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { AppHeader } from "./components/app-header/app-header";
+import { BurgerIngredients } from "./components/burger-ingredients/burger-ingredients";
 
 function App() {
   return (
     <div className="App">
       <AppHeader></AppHeader>
+      <BurgerIngredients></BurgerIngredients>
     </div>
   );
 }
---
+++ src/components/burger-ingredients/ingredients-item/ingredients-item.jsx
@@ -0,0 +1,10 @@
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+export const IngredientsItem = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
