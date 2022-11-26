a8e063d569a6526d2e4f0c2e5f1eee71b13f7680
smartfellah <smartfellah@outlook.com>
2022-11-26 16:54:35 +0300

    Add IngredientsItem styling

--- src/components/burger-ingredients/ingredients-item/ingredients-item.jsx
+++ src/components/burger-ingredients/ingredients-item/ingredients-item.jsx
@@ -1,10 +1,22 @@
-import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-export const IngredientsItem = () => {
+import {
+  CurrencyIcon,
+  Counter,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import ingredientItemStyles from "./ingredients-item.module.css";
+export const IngredientsItem = ({ data }) => {
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className={`${ingredientItemStyles["IngredientContainer"]}`}>
+      <Counter count={1} size="default" extraClass="m-1" />
+      <div className={`${ingredientItemStyles["IngredientImg"]}`}>
+        <img src={data.image} alt="" />
+      </div>
+      <div className={`${ingredientItemStyles["IngredientPrice"]}`}>
+        <p className="text text_type_digits-default">{data.price}</p>
+        <CurrencyIcon></CurrencyIcon>
+      </div>
+      <div className={`${ingredientItemStyles["IngredientName"]}`}>
+        {data.name}
+      </div>
     </div>
   );
 };
