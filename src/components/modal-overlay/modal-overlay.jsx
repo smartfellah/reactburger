8d34184504708cd6155d827afdcc9afa8e9bfb54
smartfellah <smartfellah@outlook.com>
2022-12-10 14:22:41 +0300

    Remove extra portal

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,14 +1,11 @@
 import modalOverlayStyles from "./modal-overlay.module.css";
-import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
-const modalRoot = document.getElementById("modal");
 export const ModalOverlay = ({ onClick }) => {
-  return ReactDOM.createPortal(
+  return (
     <div
       onClick={onClick}
       className={`${modalOverlayStyles["ModalOverlay"]}`}
-    ></div>,
-    modalRoot
+    ></div>
   );
 };
 ModalOverlay.propTypes = {
