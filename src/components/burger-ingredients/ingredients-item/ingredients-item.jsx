f28fb85a0a0b567f9ae1ba80e9319e7ae98b9659
smartfellah <smartfellah@outlook.com>
2022-12-23 23:14:30 +0300

    Make counter show real ammount of ingredients

--- src/components/burger-ingredients/ingredients-item/ingredients-item.jsx
+++ src/components/burger-ingredients/ingredients-item/ingredients-item.jsx
@@ -4,18 +4,38 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import ingredientItemStyles from "./ingredients-item.module.css";
 import { ingredientType } from "../../../utils/types";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import { SHOW_INGREDIENT_DETAILS } from "../../../services/actions/single-ingredient-actions";
 import { useDrag } from "react-dnd/dist/hooks";
 
 export const IngredientsItem = ({ singleIngredientData }) => {
   const dispatch = useDispatch();
+  const [amount, setAmount] = useState(0);
+  const constructorIngredients = useSelector(
+    (store) => store.constructorReducer.data
+  );
+  const constructorBun = useSelector((store) => store.constructorReducer.bun);
+
   const [, dragRef] = useDrag({
     type: "ingredient",
     item: {
       ...singleIngredientData,
     },
   });
+
+  useEffect(() => {
+    if (singleIngredientData.type === "bun") {
+      constructorBun._id === singleIngredientData._id
+        ? setAmount(2)
+        : setAmount(0);
+    } else
+      setAmount(
+        constructorIngredients.filter(
+          (ingredient) => ingredient._id === singleIngredientData._id
+        ).length
+      );
+  }, [constructorIngredients, constructorBun]);
 
   const handleClick = (e) => {
     dispatch({
@@ -37,7 +57,9 @@ export const IngredientsItem = ({ singleIngredientData }) => {
       onClick={handleClick}
       ref={dragRef}
     >
-      <Counter count={1} size="default" extraClass="m-1" />
+      {amount ? (
+        <Counter count={amount} size="default" extraClass="m-1" />
+      ) : null}
       <div className={`${ingredientItemStyles.IngredientImg}`}>
         <img src={singleIngredientData.image} alt="ingredient" />
       </div>
