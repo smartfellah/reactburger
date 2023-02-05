929d2bd2d9f724983769a4297b850637decc3e25
smartfellah <smartfellah@outlook.com>
2023-02-05 15:20:53 +0300

    Init auth reducer

---
+++ src/services/reducers/auth-reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  user: null,
+};
+
+export function authReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return {
+        ...state,
+      };
+  }
+}
