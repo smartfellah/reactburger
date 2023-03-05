0ce856fd2fd133846b5fab6a46e5d1c4f0edf76e
smartfellah <smartfellah@outlook.com>
2023-03-05 17:58:09 +0300

    refactor: :recycle: Add TS to ProtectedRouteElement and arrange imports

--- src/components/protected-route-element/protected-route-loader.tsx
+++ src/components/protected-route-element/protected-route-loader.tsx
@@ -1,4 +1,6 @@
+//UI
 import styles from "./protected-route-loader.module.css";
+
 export function ProtectedRouteLoader() {
   return <div className={styles.loader}></div>;
 }
---
+++ src/components/protected-route-element/types.ts
@@ -0,0 +1,4 @@
+export type ProtectedRouteProps = {
+  element: HTMLElement;
+  unAuthOnly: boolean;
+};
