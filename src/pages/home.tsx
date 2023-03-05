79c505ea34bef0b30215b2f1bae8a73aa739f447
smartfellah <smartfellah@outlook.com>
2023-03-05 19:33:51 +0300

    refactor: :recycle: Add TS

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,8 @@
+//Components
 import { BurgerIngredients } from "../components/burger-ingredients/burger-ingredients";
 import { BurgerConstructor } from "../components/burger-constructor/burger-constructor";
+
+//UI
 import styles from "../pages/page-styles/home.module.css";
 
 export const HomePage = () => {
