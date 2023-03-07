c61690fda45cab08cf2ddb0a495fe6e169dde806
smartfellah <smartfellah@outlook.com>
2023-03-07 09:06:22 +0300

    refactor: :recycle: Add TS

--- src/utils/check-response.ts
+++ src/utils/check-response.ts
@@ -1,4 +1,4 @@
-export const checkResponse = (response) => {
+export const checkResponse = <T>(response: Response): Promise<T> => {
   if (!response.ok) throw new Error(`Response status: ${response.status}`);
   return response.json();
 };
