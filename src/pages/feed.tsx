2a56bc3220f49b656d1687091baee3a018e37bef
smartfellah <smartfellah@outlook.com>
2023-03-30 11:43:35 +0300

    feat: :construction: Add feed page
    Started working on feed page

---
+++ src/pages/feed.tsx
@@ -0,0 +1,21 @@
+//UI
+import styles from "./page-styles/feed.module.css";
+
+export const Feed = () => {
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.leftColumn}>
+        <h1 className={styles.ordersList_Header}>Лента заказов</h1>
+        <div className={styles.ordersList_Container}></div>
+      </div>
+      <div className={styles.rightColumn}>
+        <div className={styles.currentOrders}></div>
+        <div className={styles.totalOrders}>
+          <div className={styles.ready}></div>
+          <div className={styles.inProgress}></div>
+        </div>
+        <div className={styles.todayTotal}></div>
+      </div>
+    </div>
+  );
+};
