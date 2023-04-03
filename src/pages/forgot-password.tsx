f77bf3c441ac45feac57078df75a74e813c8a460
smartfellah <smartfellah@outlook.com>
2023-04-03 21:28:31 +0300

    refactor: :recycle: Fix form

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -16,6 +16,7 @@ import { sendForgotPasswordRequest } from "../services/actions/auth-actions";
 
 //Hooks
 import { useForm } from "../hooks/useForm";
+import { FormEvent } from "react";
 
 export const ForgotPassword = () => {
   const dispatch = useDispatch();
@@ -23,18 +24,23 @@ export const ForgotPassword = () => {
 
   const { formState, handleFormChange } = useForm({ emailValue: "" });
 
-  const onRestoreClick = (): void => {
+  const onFormSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
     dispatch(sendForgotPasswordRequest(formState.emailValue, navigate));
   };
 
   return (
     <div className={`${styles["forgot-password__wrapper"]}`}>
       <div className={`${styles["forgot-password__container"]}`}>
-        <div className={`${styles["form__container"]}`}>
+        <form
+          onSubmit={onFormSubmit}
+          className={`${styles["form__container"]}`}
+          action=""
+        >
           <h2 className={`text text_type_main-medium`}>
             Восстановление пароля
           </h2>
-          <div style={{ display: "flex", flexDirection: "column" }}>
+          <div className={styles.inputContainer}>
             <EmailInput
               onChange={handleFormChange}
               value={formState.emailValue}
@@ -43,15 +49,10 @@ export const ForgotPassword = () => {
               isIcon={false}
             />
           </div>
-          <Button
-            onClick={onRestoreClick}
-            htmlType="button"
-            type="primary"
-            size="medium"
-          >
+          <Button htmlType="submit" type="primary" size="medium">
             Восстановить
           </Button>
-        </div>
+        </form>
         <div className={`${styles["options-menu__container"]}`}>
           <div className={`${styles["option__container"]}`}>
             <p className="text text_type_main-default text_color_inactive pl-10">
